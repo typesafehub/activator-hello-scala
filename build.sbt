e0d6635ce07ec531e60f23f9258377279087b832
Heiko Seeberger <heiko.seeberger@gmail.com>
2014-07-21 12:00:58 +0200

    Upgrade to ScalaTest 2.2.2, use ===
    Conflicts:
    	src/test/scala/HelloSpec.scala

--- build.sbt
+++ build.sbt
@@ -2,6 +2,6 @@ name := "hello-scala"
 
 version := "1.0"
 
-scalaVersion := "2.11.1"
+scalaVersion := "2.11.4"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.6" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.2" % "test"
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,7 +1,12 @@
-import org.scalatest._
+import org.scalatest.{ FlatSpec, Matchers }
+import org.scalactic.TypeCheckedTripleEquals
 
-class HelloSpec extends FlatSpec with Matchers {
+class HelloSpec extends FlatSpec with Matchers with TypeCheckedTripleEquals {
+
   "Hello" should "have tests" in {
-    true should be === true
+    true should ===(true)
+
+    // Because of mixing in `TypeCheckedTripleEquals` the following wouldn't compile:
+    // true should ===("true")
   }
 }
