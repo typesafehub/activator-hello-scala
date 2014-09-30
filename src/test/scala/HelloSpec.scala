07254d42392a19de560e5083f1572f876cc46d37
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-09-30 15:50:58 +0100

    use Matchers instead of ShouldMatchers

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,7 +1,6 @@
 import org.scalatest._
-import org.scalatest.matchers.ShouldMatchers
 
-class HelloSpec extends FlatSpec with ShouldMatchers {
+class HelloSpec extends FlatSpec with Matchers {
   "Hello" should "have tests" in {
     true should be === true
   }
